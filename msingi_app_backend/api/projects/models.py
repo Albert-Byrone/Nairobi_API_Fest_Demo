b24df4053766ff93c3893651140660493d294f94
Albert Byrone <albertbyrone1677@gmail.com>
2024-06-28 11:07:17 +0300

    update the model

--- msingi_app_backend/api/projects/models.py
+++ msingi_app_backend/api/projects/models.py
@@ -5,7 +5,22 @@ from ..models import AbstractBaseModel
 
 
 class Project(AbstractBaseModel):
+
     __tablename__ = 'projects'
 
-    title = Column(String(32))
+    title = Column(String)
+    image = Column(String)
     description = Column(Text)
+    location =  Column(String)
+    size =  Column(String)
+    bedrooms=  Column(String)
+    dsq =  Column(String)
+    plot_size =  Column(String)
+    prime_location =  Column(String)
+    status =Column(String)
+    experience = Column(String)
+
+
+# proj = Project(title="", description="")
+# proj.save()
+
