da4141f25772a2e1d44a1b3816dec59fe443d9fe
Albert Byrone <albertbyrone1677@gmail.com>
2024-06-28 11:07:39 +0300

    update te response schema

--- msingi_app_backend/api/projects/schemas.py
+++ msingi_app_backend/api/projects/schemas.py
@@ -1,10 +1,21 @@
 from fastapi_camelcase import CamelModel
-
+import cloudinary
+import cloudinary.uploader
 
 
 class BaseProject(CamelModel):
     title: str
+    image: str
     description: str
+    location: str
+    size: str
+    bedrooms: str
+    dsq: str
+    plot_size: str
+    prime_location: str
+    status: str
+    experience: str
+
 
 
 class CreateProject(BaseProject):
@@ -13,6 +24,15 @@ class CreateProject(BaseProject):
 class UpdateProject(CamelModel):
     title: str | None
     description: str | None
+    location: str | None
+    size: str | None
+    bedrooms: str | None
+    dsq: str | None
+    plot_size: str | None
+    prime_location: str | None
+    status: str | None
+    experience: str | None
+
 
 class GetProject(BaseProject):
     id: int
